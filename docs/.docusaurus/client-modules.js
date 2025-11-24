ca08470f4f43b75cddd77430d8ef88d7f034d884
edeink <edeity.fly@gmail.com>
2025-11-24 22:14:03 +0800

    fix: 移除 .docusaurus 运行时生成的日志

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/edeink/Documents/inkwell/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/edeink/Documents/inkwell/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.1.13_react-dom@19.1.1_react@19.1.1__react@19.1.1_typescript@5.8.3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/edeink/Documents/inkwell/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.1.13_react-dom@19.1.1_react@19.1.1__react@19.1.1_typescript@5.8.3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/edeink/Documents/inkwell/docs/src/css/custom.css"),
-];
